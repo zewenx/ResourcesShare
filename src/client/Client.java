1061d5ec71d523f5e836cadfb739b6c37f43a8e0
Francis <zw.final@gmail.com>
2017-04-02 12:25:32 +1000

    a demo client using Aaron's Server

--- src/client/Client.java
+++ src/client/Client.java
@@ -3,29 +3,48 @@ package client;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.net.Socket;
+import java.net.InetSocketAddress;
+import java.net.Socket;import java.nio.channels.NonWritableChannelException;
+import java.util.ArrayList;
 
 import VO.PublishVO;
+import VO.QueryVO;
 import VO.ResourceVO;
 
 public class Client {
 	public static void main(String[] args) {
 		Socket socket =null;
 		try {
-			socket = new Socket("127.0.0.1",8888);
+			InetSocketAddress address = new InetSocketAddress("sunrise.cis.unimelb.edu.au", 3780);
+			socket = new Socket();
+			socket.connect(address);
 			DataInputStream in = new DataInputStream( socket.getInputStream());
 			DataOutputStream out =new DataOutputStream( socket.getOutputStream());
 			PublishVO vo = new PublishVO();
-			vo.setCommand("publish");
+			vo.setCommand("PUBLISH");
 			ResourceVO resourceVO = new ResourceVO();
-			resourceVO.setDescription("google address");
-			resourceVO.setName("website");
-			resourceVO.setUri("http://www.google.com");
+//			resourceVO.setDescription("google address");
+//			resourceVO.setName("website");
+//			resourceVO.setUri("http://www.google.com");
+			
+			QueryVO queryVO = new QueryVO();
+			queryVO.setCommand("QUERY");
+			queryVO.setResourceTemplate(resourceVO);
+			queryVO.setRelay(true);
 			vo.setResource(resourceVO);
-			out.writeUTF(vo.toJson());
+			System.out.println(queryVO.toJson());
+			out.writeUTF(queryVO.toJson());
 			out.flush();
-			System.out.println(in.readUTF());
+//			while(in.available()>0)
+			Thread.sleep(50);
+			while (in.available()>0) {
+				System.out.println(in.available());
+				System.out.println(in.readUTF());
+			}
 		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}finally {
